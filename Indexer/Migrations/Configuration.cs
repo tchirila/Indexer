4d25dc0cabec759989163e6c8aacfddff06340eb
Tiberiu Chirila <tibi_chirila@yahoo.com>
2018-05-28 22:24:30 +0100

    Migration setup complete

---
+++ Indexer/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace Indexer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Indexer.Database>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(Indexer.Database context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
